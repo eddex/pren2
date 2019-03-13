345125a8c320542db5047db273aab11c1fdb9bef
iareust <kevin.reust@stud.hslu.ch>
2019-03-13 13:46:38 +0100

    Initial Commit for uart_handler
    Implementation done for:
    - TICPCommand Enumeration
    - TICPMessage Enumeration
    - TICPMessageFactory
    
    Tests writen for:
    - TICPFactory explicitly
    - Enumeration implicitly

---
+++ src/boardcomputer/uart_handler/tests/test_TICPMessageTypeFactory.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from uart_handler.ticp_common import TICPCommand, TICPMessageTypeFactory, TICPMessageType
+
+
+class TestTICPMessageTypeFactory(TestCase):
+
+    def test_single_get_ticp_message_type_from_command(self):
+
+        factory = TICPMessageTypeFactory()
+        msg_target = TICPMessageType.RES_ALL_SENSOR_DATA_MSG
+        msg_actual = factory.get_ticp_message_type_from_command(TICPCommand.RES_ALL_SENSOR_DATA)
+
+        self.assertEqual(msg_target, msg_actual)
+
+    def test_all_get_ticp_message_type_from_command(self):
+
+        factory = TICPMessageTypeFactory()
+
+        for command in TICPCommand:
+            try:
+                msg = factory.get_ticp_message_type_from_command(command)
+                if msg.command != command:
+                    break
+
+            except KeyError:
+                self.fail("No TICPMessage defined for the given TICPCommand: " + command.__str__())
+
+        else:
+            self.assertTrue(True)
---
+++ src/boardcomputer/uart_handler/ticp_common.py
@@ -0,0 +1,168 @@
+from enum import Enum
+
+import numpy as np
+
+
+class TICPCommand(Enum):
+    """Enumeration for Command Opcodes of the TICP Protocol.
+    This Class is a specialization of the Enum class.
+    Use this Enumeration für decoding 8 bit Message Blocks to the corresponding Opcode
+
+    Example:
+        The easiest way to initialize an TICPCommand Enum for a given byte is by passing
+        it to the constuctor of the Enum as follow::
+
+            $ opcode = (np.uint8) uart.pull(8)
+            $ command = TICPCommand(opcode)
+
+        or by passing an value::
+
+            $ command = TICPCommand(1)
+
+    _Git Repository:
+        https://github.com/eddex/pren2
+
+    """
+    # Request Messages (REQ)
+    REQ_ALL_SENSOR_DATA = np.uint8(0x01)
+    REQ_TOF1_DATA = np.uint8(0x02)
+    REQ_TOF2_DATA = np.uint8(0x03)
+    REQ_ACCEL_DATA = np.uint8(0x04)
+    REQ_SPEED_DATA = np.uint8(0x05)
+    REQ_SERVO_DATA = np.uint8(0x06)
+    REQ_FSM_STATE = np.uint8(0x07)
+
+    # Response Messages (RES)
+    RES_ALL_SENSOR_DATA = np.uint8(0x21)
+    RES_TOF1_SENSOR_DATA = np.uint8(0x22)
+    RES_TOF2_SENSOR_DATA = np.uint8(0x23)
+    RES_ACCEL_DATA = np.uint8(0x24)
+    RES_SPEED_DATA = np.uint8(0x25)
+    RES_SERVO_DATA = np.uint8(0x26)
+    RES_FSM_STATE = np.uint8(0x27)
+
+    # Special Commands (SPC)
+    SPC_LOOPBACK = np.uint8(0x70)
+    SPC_LOOPBACK_RESPONSE = np.uint8(0x71)
+    SPC_ACKNOWLEDGE = np.uint8(0x79)
+    SPC_START_OF_FRAME = np.uint8(0x7A)
+    SPC_ESCAPE = np.uint8(0x7D)
+    SPC_END_OF_FRAME = np.uint8(0x7E)
+
+    # Synchronization
+    SYN_MC = np.uint8(0x00)
+    SYN_BC = np.uint8(0x7F)
+
+    def __init__(self, opcode: np.uint8):
+        self._opcode: np.uint8 = opcode
+
+    @property
+    def opcode(self) -> np.uint8:
+        """ Returns a 8bit unsigned number containing the Opcoode of the Command
+
+        Returns:
+             8bit unsigned number representing the Opcoode
+        """
+        return self._opcode
+
+
+class TICPMessageType(Enum):
+    """Enumeration for Message Types of the TICP Protocol. This Class is a specialization of the Enum class.
+    Use this Enumeration for the creation of TICP Messages.
+
+    Example:
+        For the creation of TICPMessageType Enum from existing TICPCommands use the TICPCMessageTypeFactory
+
+            $ factory = TICPMessageTypeFactory()
+            $ msg = factory.get_TICPMessageType_from_TICPCommand(TICPCommand.SYN_MC)
+
+
+    _Git Repository:
+        https://github.com/eddex/pren2
+
+    """
+
+    # Request Messages REQ
+    REQ_ALL_SENSOR_DATA_MSG = (TICPCommand.REQ_ALL_SENSOR_DATA, 0)
+    REQ_TOF1_DATA_MSG = (TICPCommand.REQ_TOF1_DATA, 0)
+    REQ_TOF2_DATA_MSG = (TICPCommand.REQ_TOF2_DATA, 0)
+    REQ_ACCEL_DATA_MSG = (TICPCommand.REQ_ACCEL_DATA, 0)
+    REQ_SPEED_DATA_MSG = (TICPCommand.REQ_SPEED_DATA, 0)
+    REQ_SERVO_DATA_MSG = (TICPCommand.REQ_SERVO_DATA, 0)
+    REQ_FSM_STATE_MSG = (TICPCommand.REQ_FSM_STATE, 0)
+
+    # Response Message RES
+    RES_ALL_SENSOR_DATA_MSG = (TICPCommand.RES_ALL_SENSOR_DATA, 11)
+    RES_TOF1_SENSOR_DATA_MSG = (TICPCommand.RES_TOF1_SENSOR_DATA, 1)
+    RES_TOF2_SENSOR_DATA_MSG = (TICPCommand.RES_TOF2_SENSOR_DATA, 1)
+    RES_ACCEL_DATA_MSG = (TICPCommand.RES_ACCEL_DATA, 4)
+    RES_SPEED_DATA_MSG = (TICPCommand.RES_SPEED_DATA, 2)
+    RES_SERVO_DATA_MSG = (TICPCommand.RES_SERVO_DATA, 1)
+    RES_FSM_STATE_MSG = (TICPCommand.RES_FSM_STATE, 1)
+
+    # Special Commands SPC
+    SPC_LOOPBACK_MSG = (TICPCommand.SPC_LOOPBACK, 1)
+    SPC_LOOPBACK_RESPONSE_MSG = (TICPCommand.SPC_LOOPBACK_RESPONSE, 1)
+    SPC_ACKNOWLEDGE_MSG = (TICPCommand.SPC_ACKNOWLEDGE, 0)
+    SPC_START_OF_FRAME_MSG = (TICPCommand.SPC_START_OF_FRAME, 0)
+    SPC_ESCAPE_MSG = (TICPCommand.SPC_ESCAPE, 0)
+    SPC_END_OF_FRAM_MSG = (TICPCommand.SPC_END_OF_FRAME, 0)
+
+    # Synchronization
+    SYN_MC_MSG = (TICPCommand.SYN_MC, 0)
+    SYN_BC_MSG = (TICPCommand.SYN_BC, 0)
+
+    def __init__(self, command: TICPCommand, payload_size: int):
+        self._command: TICPCommand = command
+        self._payload_size: int = payload_size
+
+    @property
+    def command(self) -> TICPCommand:
+        """ Return the TICPCommand Enum of the TICPMessageType
+
+        :return: TICPCommand
+        """
+        return self._command
+
+    @property
+    def payload_size(self) -> int:
+        """ Returns the length of the message payload as number of byte (8bit) blocks.
+
+        :return: int
+        """
+        return self._payload_size
+
+
+class TICPMessageTypeFactory:
+    """
+    Factory for creation of TICPMessageType Enumerations
+
+    _Git Repository:
+        https://github.com/eddex/pren2
+
+    """
+
+    def __init__(self):
+        self.__command_to_msg_type_dict = {}
+        self.__init_command_to_msg_type_dict()
+
+    def __init_command_to_msg_type_dict(self) -> None:
+        """
+        Initializes a Python Dictionary for a translation of TICPCommand to TICPMessageType
+        Returns:
+
+        """
+        for msg in TICPMessageType:
+            self.__command_to_msg_type_dict[msg.command] = msg
+
+    def get_ticp_message_type_from_command(self, command: TICPCommand) -> TICPMessageType:
+        """ Factory methode for creating an TICPMessageType from a TICPCommand
+
+        Args:
+            command: The TICPCommand for which a TICPMessageType should be created
+
+        Returns:
+            The return value in form of an TICPMessageType Enum
+
+        """
+        return self.__command_to_msg_type_dict[command]
