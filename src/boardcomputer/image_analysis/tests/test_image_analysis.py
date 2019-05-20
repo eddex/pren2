ec4049b2aaa67a14b61dbe1327e08e3af32124f9
eddex <eddex@outlook.com>
2019-05-20 19:55:50 +0200

    fix build

--- src/boardcomputer/image_analysis/tests/test_image_analysis.py
+++ src/boardcomputer/image_analysis/tests/test_image_analysis.py
@@ -2,6 +2,8 @@ from mock import Mock
 import mock
 import sys
 sys.modules['openvino.inference_engine'] = Mock()
+sys.modules['picamera'] = Mock()
+sys.modules['socketio'] = Mock()
 
 from image_analysis.image_analysis import ImageAnalyzer
 
